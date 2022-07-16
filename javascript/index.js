c880601de77abcb1dc454a5f247e08e5899c3ce0
Bruna <bruna.fusiger@gmail.com>
2022-07-16 07:52:16 -0300

    carousel responsivo

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,42 @@
-
 //carousel
 
-var $simpleCarousel = document.querySelector(".container");
+const simpleCarousels = document.querySelectorAll(".container");
 
-new Glider($simpleCarousel, {
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  draggable: true,
-  arrows: {
-    prev: ".back",
-    next: ".go",
-  },
-  scrollLock: true,
-  rewind: true
-});
+for (var i = 0; i < simpleCarousels.length; i++) {
+  new Glider(simpleCarousels[i], {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    arrows: {
+      prev: `#back${i}`,
+      next: `#go${i}`,
+    },
+    scrollLock: true,
+    rewind: true,
+  });
+}
 
 // click-table
 
-const message = document.querySelector(".message")
+// const messages = document.querySelectorAll(".container-message");
 
-function tableClick() {
-   alert("ainda vou arrumar isso");
+// for (var m = 0; m < messages.length; m++) {
+//   function tableClick(mensagem) { 
+//     const messageX = document.querySelector(`#message${m}`);
+//     const message = messages[m];
+
+//     message.textContent = mensagem;
+//   }
+// }
+
+const message = document.querySelector("#message0");
+
+function tableClick(mensagem) {
+  message.textContent = mensagem;
 }
 
-//passar mouse por cima
+tableClick(message);
 
 
+
+// passar mouse por cima
